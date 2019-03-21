821730e4a309acfe5688913544a9cb07bf932a45
Jennifer Lipps <jjlipps@gmail.com>
2019-03-20 22:48:36 -0500

    wip

--- main.js
+++ main.js
@@ -6,10 +6,19 @@ const printToDom = (divId, textToPrint) => {
 const button = document.getElementById("convertBtn");
 
 const determineConverter = (e) => {
-  let inputValue = document.getElementById("tempInput").value;
+  let temp = document.getElementById("tempInput").value;
   let degreeValue = document.querySelector('input[name="degreeType"]:checked').value;
-  console.log(degreeValue, inputValue);
-  printToDom('tempOutput', inputValue);
+  const toFahrenheit = ((temp * (9/5)) + 32);
+  const toCelsius = ((temp - 32) * (5/9));
+
+  if (degreeValue === "fahrenheit") {
+    // console.log(degreeValue, temp);
+    printToDom('tempOutput', toFahrenheit);
+  } else {
+      if (degreeValue === "celsius") {
+        printToDom('tempOutput', toCelsius);
+      };
+  };
 }
 
 button.addEventListener("click", determineConverter);
