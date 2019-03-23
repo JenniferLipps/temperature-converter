0fb1a52c66e4b320e80e1cc68cc40dbcb6c2a606
Jennifer Lipps <jjlipps@gmail.com>
2019-03-23 11:14:20 -0500

    completed temperature conversion

--- main.js
+++ main.js
@@ -5,20 +5,35 @@ const printToDom = (divId, textToPrint) => {
 
 const button = document.getElementById("convertBtn");
 
-const determineConverter = (e) => {
+const toCelsius = () => {
   let temp = document.getElementById("tempInput").value;
-  let degreeValue = document.querySelector('input[name="degreeType"]:checked').value;
-  const toFahrenheit = ((temp * (9/5)) + 32);
-  const toCelsius = ((temp - 32) * (5/9));
+  const finalTemp = ((temp - 32) * (5/9));
+  domStringBuilder(finalTemp, "C");
+};
 
+const toFahrenheit = () => {
+  let temp = document.getElementById("tempInput").value;
+  const finalTemp = ((temp * (9/5)) + 32);
+  domStringBuilder(finalTemp, "F");
+};
+
+const domStringBuilder = (finalTemp, unit) => {
+  let domString = '';
+    domString += '<h2>'
+      domString += finalTemp
+      domString += unit
+    domString += '</h2>'
+  printToDom('tempOutput', domString); 
+};
+
+const determineConverter = (e) => {
+  let degreeValue = document.querySelector('input[name="degreeType"]:checked').value;
   if (degreeValue === "fahrenheit") {
-    // console.log(degreeValue, temp);
-    printToDom('tempOutput', toFahrenheit);
+    toCelsius();
   } else {
-      if (degreeValue === "celsius") {
-        printToDom('tempOutput', toCelsius);
-      };
+    toFahrenheit();
   };
 }
 
 button.addEventListener("click", determineConverter);
+
